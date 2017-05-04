a6a6446aff09c54df11faa26654a39dfa687b11e
TWH <dasvasas@gmail.com>
2017-05-04 15:12:23 +0300

    Many edits

--- app/Http/Controllers/Admin/InterfaceController.php
+++ app/Http/Controllers/Admin/InterfaceController.php
@@ -21,36 +21,25 @@ class InterfaceController extends Controller
 {
     // 10080 минут - 1 неделя
     const SHOWADMIN_CACHE_TTL = 10080;
-//    const SHOWALBUMS_COUNTER_PAGE = 10;
     
     /*
      * Отображение общего списка групп/альбомов на странице администратора
      */
     public function getPage(){
         
-//        if(Groups::count() == 0) {
-//            $groups = [];
-//            $groupsArray = [];
-//        } else {
-            $groups = Cache::remember(sha1('admin.show.groups'), self::SHOWADMIN_CACHE_TTL, function() {
-                return Groups::All();
-            });
-            $groupsArray = Cache::remember(sha1('admin.show.groupsArray'), self::SHOWADMIN_CACHE_TTL, function() {
-                return Groups::orderBy('name')->pluck('name','id');
-            });
-//        }
-        
         $thumbs_dir = Setting::get('thumbs_dir');
         
-//        if(Albums::count() == 0) {
-//            $albums=[];
-//        } else {
-            
-            $albums = Cache::remember(sha1('admin.show.albums'), self::SHOWADMIN_CACHE_TTL, function() {
-                return Albums::all();
-            });
-            
-//        }
+        $groups = Cache::remember(sha1('admin.show.groups'), self::SHOWADMIN_CACHE_TTL, function() {
+            return Groups::All();
+        });
+        
+        $groupsArray = Cache::remember(sha1('admin.show.groupsArray'), self::SHOWADMIN_CACHE_TTL, function() {
+            return Groups::orderBy('name')->pluck('name','id');
+        });
+        
+        $albums = Cache::remember(sha1('admin.show.albums'), self::SHOWADMIN_CACHE_TTL, function() {
+            return Albums::all();
+        });
         
         $resultData = compact(
             'groupsArray',
