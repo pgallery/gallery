b905d07a06725fe1e8277bb7bb5d7e97227884c9
TWH <dasvasas@gmail.com>
2017-09-26 19:21:34 +0300

    fix .env.example

--- database/seeds/baseUsers.php
+++ database/seeds/baseUsers.php
@@ -23,21 +23,11 @@ class baseUsers extends Seeder
             'password'  => Hash::make('admin'),
             'method'    => 'thisSite',
         ]);
-        User::create([
-            'name'      => 'Moder',
-            'email'     => 'moder@example.com',
-            'password'  => Hash::make('moder'),
-            'method'    => 'thisSite',
-        ]);
         
         $RoleAdmin = Roles::select('id')->where('name', 'admin')->first();
-        $RoleModer = Roles::select('id')->where('name', 'moderator')->first();
         
         $user = User::find(1);
         $user->roles()->attach($RoleAdmin->id);
-
-        $user = User::find(2);
-        $user->roles()->attach($RoleModer->id);
         
     }
 }
